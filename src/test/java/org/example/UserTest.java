34cab9f3b7e9f105267f18311d11ff19ab828eaa
Dongyeong Kwon <join262@naver.com>
2021-04-07 10:52:22 +0900

    Uploaded Maven related file

---
+++ src/test/java/org/example/UserTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UserTest {
+    @Test
+    public void testAskPreference() {
+        // assert
+    }
+    @Test
+    public void testRecommendation() {
+
+    }
+    @Test
+    public void testMatching() {
+
+    }
+    @Test
+    public void testProvide() {
+
+    }
+}
