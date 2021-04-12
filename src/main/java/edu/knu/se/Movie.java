9693c24239b8e930e33cefb286ec98bfab084894
kangsan <ogs0518@gmail.com>
2021-04-12 21:28:21 +0900

    add UIclass, getmoviemethods

--- src/main/java/edu/knu/se/Movie.java
+++ src/main/java/edu/knu/se/Movie.java
@@ -3,31 +3,29 @@ package edu.knu.se;
 public class Movie {
 	private int movieId;
 	private String title;
-	private String genre;
+	private int genre;
 	private String director;
 	
-	public Movie(int movieId, String title, String genre, String director) {
+	public Movie(int movieId, String title, int genre, String director) {
 		this.movieId = movieId;
 		this.title = title;
 		this.genre = genre;
 		this.director =director;
 	}
 
-	public String toString() {
-		return "Movie{" +
-				"movieId=" + movieId +
-				", title='" + title + '\'' +
-				", genre='" + genre + '\'' +
-				", director='" + director + '\'' +
-				'}';
+	public int getMovieId() {
+		return movieId;
 	}
 
+	public String getTitle() {
+		return title;
+	}
 
-	//
-//	public String loadMovieInfo() {
-//		String titleGenreDirector = title + genre + director;
-//		return titleGenreDirector;
-//	}
-	
-	
+	public int getGenre() {
+		return genre;
+	}
+
+	public String getDirector() {
+		return director;
+	}
 }
---
+++ src/main/java/edu/knu/se/UI.java
@@ -0,0 +1,10 @@
+package edu.knu.se;
+
+public class UI {
+    public boolean provideMovieInfo(){
+        return true;
+    }
+    public boolean askPreferenceWindow(){
+        return true;
+    }
+}
