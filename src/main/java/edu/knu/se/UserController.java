903b5ddfae0c6320d0781d34875a668e2a4fe758
윤경록 <mason@yungyeonglog-ui-MacBook-Pro.local>
2021-05-26 12:47:21 +0900

    User controller changed

--- src/main/java/edu/knu/se/UserController.java
+++ src/main/java/edu/knu/se/UserController.java
@@ -14,6 +14,8 @@ class user_movies {
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    BufferedWriter bw = null;
+    BufferedWriter bwuser = null;
     private final UserRepository repository;
     List<String> data; 
     UserController(UserRepository repository){
@@ -56,24 +58,71 @@ public class UserController {
     @PutMapping("/{userid}")
     String putUser(@PathVariable(name="userid") String uid, @RequestParam(name="passwd") String pwd) {
         String result = "FAILED";
-        if(!(repository.existsById(uid))) {
-            User newUser = new User();
-            newUser.setUid(uid);
-            newUser.setPasswd(pwd);
-            repository.save(newUser);
-            result = "SUCCESS";
-        } else {
-            repository.findById(uid)
-                    .map(user -> {
-                        user.setUid(uid);
-                        user.setPasswd(pwd);
-                        repository.save(user);
-                        return "";
-                    });
-            result = "SUCCESS";
+        try {
+            File csv_user = new File("Users.csv");
+            bwuser = new BufferedWriter(new FileWriter(csv_user));
+            if (!(repository.existsById(uid))) {
+
+                User newUser = new User();
+                newUser.setUid(uid);
+                newUser.setPasswd(pwd);
+                repository.save(newUser);
+                result = "SUCCESS";
+            } else {
+                repository.findById(uid)
+                        .map(user -> {
+                            user.setUid(uid);
+                            user.setPasswd(pwd);
+                            repository.save(user);
+                            return "";
+                        });
+                result = "SUCCESS";
+            }
+        } catch(FileNotFoundException e){
+
+        } catch(IOException e){
+
         }
         return "{\"result\":"+result+"\"}";
     }
+
+    @PutMapping("/{uid}/ratings")
+    String Testing(@PathVariable("uid")String uid,@RequestParam("movie") String movie_id,@RequestParam("rating") int rating){
+        String result = "FAILED";
+
+
+        if(!(rating >= 1 && rating <= 5)) return result;
+
+        try {
+            File csv_write = new File("User_Rating_DB.csv");
+            File csv_user = new File("Users.csv");
+            File csv = new File("movies.csv");
+            bwuser = new BufferedWriter(new FileWriter(csv_user));
+            bw = new BufferedWriter(new FileWriter(csv_write));
+            BufferedReader brd = new BufferedReader((new FileReader(csv_write));
+
+            BufferedReader br = new BufferedReader(new FileReader(csv));
+            String line = "";
+            String d_line = "";
+            while ((line = br.readLine()) != null){
+                String[] token = line.split(",", -1);
+                if(token[0].equals(movie_id))
+                {
+                    bw.write(uid);
+                    bw.write(',');
+                    bw.write(rating);
+                    bw.write('\n');
+                }
+            }
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
 
     @DeleteMapping("/{userid}")
     String deleteUid(@PathVariable(name="userid") String uid) {
