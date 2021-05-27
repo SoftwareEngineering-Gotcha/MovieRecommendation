33dade357944a8b8456ce812be9f81b023dd7396
Dongyeong Kwon <join262@naver.com>
2021-05-27 20:13:53 +0900

    Edited UserController and Added UserCraationControl

--- src/main/java/edu/knu/se/UserController.java
+++ src/main/java/edu/knu/se/UserController.java
@@ -3,7 +3,12 @@ import java.io.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.core.io.*;
 
-import java.nio.Buffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Timestamp;
 import java.util.*;
 
 class user_movies {
@@ -18,57 +23,80 @@ class user_movies {
 public class UserController {
     BufferedWriter bw = null;
     BufferedWriter bwuser = null;
-    BufferedReader read_bwuser = null;
     private final UserRepository repository;
-    List<String> data; 
+    List<String> data;
     UserController(UserRepository repository){
         this.repository = repository;
     }
 
     @GetMapping("")
     List<User> all(){
-        return repository.findAll();
+        Path path = Paths.get("user.csv");
+        Charset cs = StandardCharsets.UTF_8;
+        ArrayList<User> result = new ArrayList<>();
+        List<String> list = new ArrayList<String>();
+        User temp;
+
+        try{
+            list = Files.readAllLines(path,cs);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        String[] data;
+        for(int i = 0 ; i < list.size(); i++){
+            data = list.get(i).split(",");
+            // System.out.println(list.get(i));
+            // System.out.println(data);
+            if (i != 0) {
+                temp = new User(data[0], data[1], null, null);
+                result.add(temp);
+            }
+        }
+        // return result;
+        return result;
     }
 
     @GetMapping("/_count_")
     int count(){
-        int count = 0;
+        Path path = Paths.get("user.csv");
+        Charset cs = StandardCharsets.UTF_8;
+        ArrayList<User> result = new ArrayList<>();
+        List<String> list = new ArrayList<String>();
+        User temp;
+
         try{
-            File csv_user = new File("user.csv");
-            read_bwuser = new BufferedReader(new FileReader(csv_user));
-            String line="";
-            while((line = read_bwuser.readLine()) != null){
-                count++;
+            list = Files.readAllLines(path,cs);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        String[] data;
+        for(int i = 0 ; i < list.size(); i++){
+            data = list.get(i).split(",");
+            // System.out.println(list.get(i));
+            // System.out.println(data);
+            if (i != 0) {
+                temp = new User(data[0], data[1], null, null);
+                result.add(temp);
             }
-        }catch (FileNotFoundException e){
+        }
 
-        }catch (IOException e){}
-
-        count--;
-        return count;
+        return list.size() - 1;
     }
 
-    @GetMapping("/{userid}/ratings")
+    @GetMapping("/{userid}/ratings") // rating만 뜨도록 설정 완료
     List<String> return_movie(@PathVariable(name = "userid") String uid){
-        boolean user_exist = false;
         try {
             File csv = new File("ratings.csv");
-            File csv_user = new File("user.csv");
             data = new ArrayList<String>();
             BufferedReader br = new BufferedReader(new FileReader(csv));
-            BufferedReader bru = new BufferedReader(new FileReader(csv_user));
             String line = "";
-            String line_u = "";
-            while((line_u = bru.readLine()) != null){
-                String [] token = line.split(",", -1);
-                if(token[0].equals(uid)) user_exist = true;
-            }
-            if(user_exist == false) return data;
             while ((line = br.readLine()) != null){
                 String[] token = line.split(",", -1);
                 if(token[0].equals(uid))
                 {
-                    data.add(token[1]);
+                    data.add(token[2]);
                 }
             }
         } catch(FileNotFoundException e){
@@ -82,42 +110,87 @@ public class UserController {
     @PutMapping("/{userid}")
     String putUser(@PathVariable(name="userid") String uid, @RequestParam(name="passwd") String pwd) {
         String result = "FAILED";
-        try {
-            File csv_user = new File("user.csv");
-            read_bwuser = new BufferedReader(new FileReader(csv_user));
-            bwuser = new BufferedWriter(new FileWriter(csv_user,true));
-            String line ="";
-            while((line = read_bwuser.readLine()) != null){
-                String[] token = line.split(",", -1);
-                if(token[0].equals(uid)){
-                    return "{\"result\":"+result+"\"}";
-                }
+        List<String> list = new ArrayList<String>();
+
+        try{
+            File file = new File("user.csv");
+            FileReader filereader = new FileReader(file);
+            BufferedReader bufReader = new BufferedReader(filereader);
+            String line = "";
+            while((line = bufReader.readLine()) != null){
+                System.out.println(line);
+                list.add(line);
             }
-            bwuser.write(uid);
-            bwuser.write(',');
-            bwuser.write(pwd);
-            bwuser.write('\n');
-            result = "SUCCESS";
-            bwuser.flush();
-            bwuser.close();
-        } catch(FileNotFoundException e){
-
-        } catch(IOException e){
-
+            bufReader.close();
+        }catch (FileNotFoundException e) {
+            return "{\"result\":"+result+"\"}";
+        }catch(IOException e){
+            return "{\"result\":"+result+"\"}";
         }
+
+        String[] data;
+        for(int i = 0 ; i < list.size(); i++){ // list에 있는 string의 data를 추출해서 uid를 비교
+            data = list.get(i).split(",");
+            System.out.println(list.get(i));
+            System.out.println(data[0] + "," + data[1]);
+            if (uid.equals(data[0])) { // 같은 게 있으면 안됨
+                return "{\"result\":"+result+"\"}";
+            }
+        }
+
+        list.add(uid + "," + pwd);
+        // 필요하면 add하고 sort까지 넣을 것
+
+        try{
+            File file = new File("user.csv");
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+
+            if(file.isFile() && file.canWrite()){
+                for(int i = 0; i < list.size() ; i++) {
+                    System.out.println(list.get(i));
+                    bufferedWriter.write(list.get(i));
+                    bufferedWriter.newLine();
+                    System.out.println("Write Success");
+                }
+                bufferedWriter.flush();
+                bufferedWriter.close();
+            }
+        } catch (IOException e) {
+            return "{\"result\":"+result+"\"}";
+        }
+
+        result = "SUCCESS";
         return "{\"result\":"+result+"\"}";
     }
 
-    @PutMapping("/{uid}/ratings")
-    String Testing(@PathVariable("uid")String uid,@RequestParam("movie") String movie_id,@RequestParam("rating") int rating){
+    @PutMapping("/{uid}/ratings") // 400에러 떳음 확인 필요
+    String Testing(@PathVariable("uid")String uid,@RequestParam("movie") String movie_id,@RequestParam("rating") float rating){
         String result = "FAILED";
+        List<String> movielist = new ArrayList<String>();
 
+        if(!(rating >= 1 && rating <= 5)) return "{\"result\":"+result+"\"}";
 
-        if(!(rating >= 1 && rating <= 5)) return result;
+        if((rating%0.5) != 0.0) return "{\"result\":"+result+"\"}";
+
+        try{
+            File file = new File("user.csv");
+            FileReader filereader = new FileReader(file);
+            BufferedReader bufReader = new BufferedReader(filereader);
+            String line = "";
+            while((line = bufReader.readLine()) != null){
+                System.out.println(line);
+                movielist.add(line);
+            }
+            bufReader.close();
+        }catch (FileNotFoundException e) {
+            return "{\"result\":\"" + result + "\"}";
+        }catch(IOException e){
+            return "{\"result\":\"" + result + "\"}";
+        }
 
         try {
             File csv_write = new File("ratings.csv");
-            File csv_user = new File("Users.csv");
+            File csv_user = new File("user.csv");
             File csv = new File("movies.csv");
             bwuser = new BufferedWriter(new FileWriter(csv_user));
             bw = new BufferedWriter(new FileWriter(csv_write,true));
@@ -127,7 +200,7 @@ public class UserController {
             String line = "";
             String d_line = "";
 
-            String s_rating = new String(Integer.toString(rating));
+            String s_rating = new String(Float.toString(rating));
             while ((line = br.readLine()) != null){
                 String[] token = line.split(",", -1);
                 if(token[0].equals(movie_id))
@@ -138,6 +211,8 @@ public class UserController {
                     bw.write(',');
                     bw.write(s_rating);
                     bw.write(',');
+                    long temp = new Timestamp(System.currentTimeMillis()).getTime();
+                    bw.write(Long.toString(temp / 1000));
                     bw.write('\n');
                     result = "SUCCESS";
                     bw.flush();
@@ -150,32 +225,67 @@ public class UserController {
             e.printStackTrace();
         }
 
-
-        return result;
+        return "{\"result\":"+result+"\"}";
     }
 
 
-
     @DeleteMapping("/{userid}")
-    String deleteUid(@PathVariable(name="userid") String uid) {
+    String deleteUid(@PathVariable(name="userid") String uid) throws FileNotFoundException {
         String result = "FAILED";
+
+        List<String> list = new ArrayList<String>();
+
         try{
-            File csv_user = new File("Users.csv");
-            read_bwuser = new BufferedReader(new FileReader(csv_user));
-            bwuser = new BufferedWriter(new FileWriter(csv_user,true));
-            String buffer = "";
-            while((buffer = read_bwuser.readLine()) != null){
-                String[] token = buffer.split(",", -1);
+            File file = new File("user.csv");
+            FileReader filereader = new FileReader(file);
+            BufferedReader bufReader = new BufferedReader(filereader);
+            String line = "";
+            while((line = bufReader.readLine()) != null){
+                System.out.println(line);
+                list.add(line);
             }
-        } catch(FileNotFoundException e){
-
-        } catch(IOException e){
-
+            bufReader.close();
+        }catch (FileNotFoundException e) {
+            return "{\"result\":\"" + result + "\"}";
+        }catch(IOException e){
+            return "{\"result\":\"" + result + "\"}";
         }
-        if((repository.existsById(uid))) {
-            repository.deleteById(uid);
-            result = "SUCCESS";
+
+        String[] data;
+        for(int i = 0 ; i < list.size(); i++){ // list에 있는 string의 data를 추출해서 uid를 비교
+            data = list.get(i).split(",");
+            System.out.println(list.get(i));
+            System.out.println(data[0] + "," + data[1]);
+            if (uid.equals(data[0])) { // 같은 게 있으면 제거
+                result = "SUCCESS";
+                list.remove(i);
+                break;
+            }
         }
+
+        System.out.println("After removed");
+        for(int i = 0; i < list.size() ; i++) {
+            System.out.println(list.get(i));
+        }
+
+        try{
+            File file = new File("user.csv");
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+
+            if(file.isFile() && file.canWrite()){
+                for(int i = 0; i < list.size() ; i++) {
+                    System.out.println(list.get(i));
+                    bufferedWriter.write(list.get(i));
+                    bufferedWriter.newLine();
+                    System.out.println("Write Success");
+                }
+                bufferedWriter.flush();
+                bufferedWriter.close();
+            }
+        } catch (IOException e) {
+            return "{\"result\":\"" + "FAILED" + "\"}";
+        }
+
         return "{\"result\":\"" + result + "\"}";
     }
 }
